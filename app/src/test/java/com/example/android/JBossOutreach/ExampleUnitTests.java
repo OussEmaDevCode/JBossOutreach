26d25eaed8bfe63b12179b303549979d049e43b9
oussama <oussamahassini6@gmail.com>
2018-11-14 18:14:01 +0100

    Added the Unit tests

---
+++ app/src/test/java/com/example/android/JBossOutreach/ExampleUnitTests.java
@@ -0,0 +1,16 @@
+package com.example.android.JBossOutreach;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTests {
+    @Test
+    public void ExampleTests() throws IOException {
+        assertEquals(4, 2 + 2);
+
+    }
+
+}
